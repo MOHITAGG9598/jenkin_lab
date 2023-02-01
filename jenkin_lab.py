9583cd090896338066f6cb195180fe9b7d82790e
Mohit <119918426+MOHITAGG9598@users.noreply.github.com>
2023-02-01 22:23:40 +0530

    commit for better understanding

---
+++ jenkin_lab.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name1="Hello i'm Mohit Agarwal"
+name2="This is my Jenkin Lab evaluation"
+Result=name1+'_'+name2
+print(Result)
+
+
+# In[ ]:
+
+
+
+
